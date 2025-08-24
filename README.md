ab6a7c2e33af46e74b42caf8bfe93c7dc15622e8
bddjr <bddjr08@outlook.com>
2025-08-25 01:10:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# westddns-py
+
+[西部数码](https://www.west.cn)专用的 DDNS 脚本。
+
+## 在 Linux 安装
+
+依赖：
+> python3  
+> git  
+
+1. 克隆仓库并设置权限
+
+```bash
+sudo bash -c "git clone https://github.com/bddjr/westddns-py /opt/westddns-py && chmod 700 /opt/westddns-py"
+```
+
+2. 创建并填写配置文件。  
+   `apidomainkey`请参考 <https://www.west.cn/docs/505763.html> 填写。
+
+```bash
+sudo nano /opt/westddns-py/conf.json
+```
+
+```json
+{
+    "apidomainkey": "",
+    "domain": "example.com",
+    "hostname": "ddns.example.com",
+    "get_ip_from": "https://4.ipw.cn"
+}
+```
+
+3. 创建服务
+
+```bash
+sudo cp /opt/westddns-py/westddns.service /etc/systemd/system/
+sudo systemctl daemon-reload
+```
+
+4. 运行服务（开机自启）
+
+```bash
+sudo systemctl enable --now westddns
+```
---
+++ westddns.py
@@ -0,0 +1,41 @@
+from urllib.parse import quote
+import requests, json, time, logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s %(asctime)s] %(message)s',
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+logging.info('westddns-py start!')
+
+with open('conf.json') as f:
+    conf = json.load(f)
+
+domain = quote(str(conf['domain']))
+hostname = quote(str(conf['hostname']))
+apidomainkey = quote(str(conf['apidomainkey']))
+
+ipa = None
+
+try:
+    while True:
+        try:
+            resp = requests.get(conf['get_ip_from'])
+            if resp.ok:
+                respText = resp.text.strip()
+                if ipa != respText:
+                    ipa = respText
+                    logging.info(ipa)
+                    record_value = quote(str(ipa))
+                    resp = requests.get(f'https://api.west.cn/API/v2/domain/dns/?act=dnsrec.update&domain={domain}&hostname={hostname}&record_value={record_value}&apidomainkey={apidomainkey}')
+                    if resp.ok:
+                        logging.info('Set DNS record success!')
+                    else:
+                        logging.error(f'Set DNS record failed! Code: {resp.status_code}')
+            else:
+                logging.error(f'Get IP address failed! Code: {resp.status_code}')
+        except Exception as e:
+            logging.exception(e)
+        time.sleep(60)
+except KeyboardInterrupt:
+    print('^C')
